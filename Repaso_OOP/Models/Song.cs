e0fb34bdb386c95df9ca7630f8d916a21d7f5cf1
Carlos Ramos <carlosrc78@gmail.com>
2025-04-14 23:01:23 -0500

    Repaso

---
+++ Repaso_OOP/Models/Song.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso_OOP.Clases
+{
+    internal class Song
+    {
+        private string _name; // access modifier
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+        private string artist;
+        private string genre;
+        private string albumName;
+        private int durationSeconds;
+        public Song(string name, string artist, string genre, string albumName, int durationSeconds)
+        {
+            this.name = name;
+            this.artist = artist;
+            this.genre = genre;
+            this.albumName = albumName;
+            this.durationSeconds = durationSeconds;
+        }
+
+        public Song(string name, string artist, string albumName, int durationSeconds)
+        {
+            this.name = name;
+            this.artist = artist;
+            this.albumName = albumName;
+            this.durationSeconds = durationSeconds;
+        }
+
+
+        public string mostrarNombreCancion(string nombre)
+        {
+            return name + "" + nombre;
+
+        }
+
+    }
+}
--- Repaso_OOP/Program.cs
+++ Repaso_OOP/Program.cs
@@ -1,21 +1,36 @@
-﻿namespace Repaso_OOP
+﻿using Repaso_OOP.Clases;
+
+namespace Repaso_OOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Song song = new Song();
+            Song song = new Song("Something About Us","Daft Punk","Electronic","Discovery",240);
             Console.WriteLine(song.name);
+            Console.WriteLine(song.artist);
+            Console.WriteLine(song.genre);
+            Console.WriteLine(song.albumName);
+            Console.WriteLine(song.durationSeconds);
+
+            Song song2 = new Song("Around the Word", "Daft Punk", "Electronic", "Discovery", 240);
+            Console.WriteLine(song.name);
+            Console.WriteLine(song.artist);
+            Console.WriteLine(song.genre);
+            Console.WriteLine(song.albumName);
+            Console.WriteLine(song.durationSeconds);
+
+            Song song3 = new Song("Get Lucky",  "Electronic", "Ramdom Acces Memories", 240);
+            Console.WriteLine(song.name);
+            Console.WriteLine(song.artist);
+            Console.WriteLine(song.genre);
+            Console.WriteLine(song.albumName);
+            Console.WriteLine(song.durationSeconds);
+            song3.genre = "Electronic";
         }
 
-        public class Song
-        {
-            public string name;
-            public string artist;
-            public string genre;
-            public string albumName;
-            public int durationSeconds;
-        }
+        
+      
     }
 }
