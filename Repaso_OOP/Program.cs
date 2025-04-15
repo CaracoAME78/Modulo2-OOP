3001d34fa76f9731b296abd3ff0fd8e9bd07dd53
Carlos Ramos <carlosrc78@gmail.com>
2025-04-14 22:34:03 -0500

    Agregar archivos de proyecto.

---
+++ Repaso_OOP/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Repaso_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Song song = new Song();
+            Console.WriteLine(song.name);
+        }
+
+        public class Song
+        {
+            public string name;
+            public string artist;
+            public string genre;
+            public string albumName;
+            public int durationSeconds;
+        }
+    }
+}
